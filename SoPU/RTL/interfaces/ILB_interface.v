25d0fb22fad58d8a326a6946b4ab91ff1e68beb8
khajanh1 <hkhajanchi97@att.net>
2020-02-13 13:50:23 -0500

    Added update SoPU interfaces

--- SoPU/RTL/interfaces/ILB_interface.v
+++ SoPU/RTL/interfaces/ILB_interface.v
@@ -19,6 +19,24 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+/*
+
+    This module will interface with the ILB with two transactions
+    
+    - Send the newest uart pixel to the ILB (Transaction I) 
+        - Interface signals: 
+            - Input: sop_to_ilb_rts_I
+            - Output: sop_to_ilb_rtr_I
+            
+    - Get old pixel data from ILB
+        - Interface signals: 
+            - Input: sop_to_ilb_rtr_II
+            - Output: sop_to_ilb_rts_II
+
+- Uses a simple FSM to cycle through the states, and assert 'bytes_received' when the old pixel data is recieved
+
+*/
+
 //Interface to read line buffer values in
 module ILB_interface_in
 (
@@ -26,15 +44,26 @@ module ILB_interface_in
 input clk,
 input rst, 
 
-output reg sop_to_ilb_rtr, 
+/* Interface Signals */ 
+output reg sop_to_ilb_rtr_II, 
+input sop_to_ilb_rts_II, 
+
+input sop_to_ilb_rtr_I,
+output reg sop_to_ilb_rts_I,
+
+
+/* From Master FSM controller */
 output reg bytes_recieved, 
 
-input sop_to_ilb_rts, 
 input ilb_read_enable, 
+input ilb_send_enable, 
+
+/* Input/output bytes from UART to ILB */ 
+input      [7:0] uart_byte,
+output reg [7:0] output_byte,
 
 
-
-
+/* Input pixel values from ILB 8*/
 input [7:0] ilb_byte_0,
 input [7:0] ilb_byte_1,
 input [7:0] ilb_byte_2,
@@ -42,6 +71,7 @@ input [7:0] ilb_byte_3,
 input [7:0] ilb_byte_4,
 input [7:0] ilb_byte_5,
 
+/* Output pixel values to IM_Window */ 
 output reg  [7:0] byte_0,
 output reg  [7:0] byte_1,
 output reg  [7:0] byte_2,
@@ -52,15 +82,33 @@ output reg  [7:0] byte_5
 
 );
 
+/*
+
+- Send byte to ILB
+
+    - interface_rts <= 1;
+    
+    - wait for ILB_rtr == 1
+    
+    - when ILB_rtr == 1
+        - output_byte <= input_byte; 
+        
+    - ilb_rts       <= 0; 
+    
+- Wait for ILB rts
+- get bytes 
 
 
-
-localparam IDLE  = 3'b0;
-localparam WAIT_FOR_DATA = 3'b1;
-localparam LATCH_BYTES = 3'd2 ;
-
-reg [3:0] state; 
  
+*/
+
+
+localparam IDLE           = 3'd0 ;
+localparam SEND_BYTE_ILB  = 3'd1 ; 
+localparam READ_ILB_BYTES = 3'd2 ;
+localparam LATCH_BYTES    = 3'd3 ;
+
+reg [3:0] state;  
 reg [2:0] ctr; 
 
  
@@ -70,8 +118,10 @@ begin
     if (!rst) 
         begin
         
-        sop_to_ilb_rtr <= 0; 
-        bytes_recieved <= 0; 
+        sop_to_ilb_rtr_II <= 0;
+        sop_to_ilb_rts_I  <= 0; 
+         
+        bytes_recieved    <= 0; 
         
         byte_0 <= 0; 
         byte_1 <= 0; 
@@ -80,6 +130,7 @@ begin
         byte_4 <= 0; 
         byte_5 <= 0; 
         
+        output_byte <= 0; 
         
         state <= IDLE; 
         
@@ -94,8 +145,10 @@ begin
             
                 IDLE: begin
                         
-                    sop_to_ilb_rtr <= 0; 
-                    bytes_recieved <= 0; 
+                    sop_to_ilb_rtr_II <= 0; 
+                    sop_to_ilb_rts_I  <= 0;
+                    
+                    bytes_recieved    <= 0; 
                 
                     byte_0 <= 0; 
                     byte_1 <= 0; 
@@ -104,22 +157,30 @@ begin
                     byte_4 <= 0; 
                     byte_5 <= 0; 
                     
-                  
+                    output_byte <= 0; 
+                    
                     ctr <= 0; 
                         
-                        if (ilb_read_enable) 
-                            state <= WAIT_FOR_DATA; 
+                        if (ilb_send_enable) 
+                            state <= SEND_BYTE_ILB; 
                         else 
                             state <= IDLE; 
                         
                 
                             
                       end //IDLE case end
+                      
+                      
+                                  
        
-                WAIT_FOR_DATA: begin
+                SEND_BYTE_ILB: begin
                 
-                                sop_to_ilb_rtr <= 0; 
-                                bytes_recieved <= 0; 
+                                sop_to_ilb_rts_I  <= 1; 
+                                sop_to_ilb_rtr_II <= 0; 
+                                bytes_recieved    <= 0; 
+                                output_byte       <= uart_byte; 
+                                ctr               <= 0; 
+
                             
                                 byte_0 <= 0; 
                                 byte_1 <= 0; 
@@ -127,38 +188,69 @@ begin
                                 byte_3 <= 0; 
                                 byte_4 <= 0; 
                                 byte_5 <= 0; 
-                
-                                if (sop_to_ilb_rts)
-                                    state <= LATCH_BYTES; 
+                                    
+                                
+                                if (sop_to_ilb_rtr_I)
+                                    begin
+                                        state <= READ_ILB_BYTES; 
+                                    end 
                                 else    
-                                    state <= WAIT_FOR_DATA; 
+                                    state <= SEND_BYTE_ILB; 
                                     
                 
                 
                                end //Wait for data case end
                                
-                LATCH_BYTES:    begin
+                READ_ILB_BYTES:    begin
                                 
-                                sop_to_ilb_rtr <= 0; 
-                                bytes_recieved <= 1;  
-                               
+                                sop_to_ilb_rts_I  <= 0;
+                                sop_to_ilb_rtr_II <= 1; 
+                                bytes_recieved    <= 0;  
+                                output_byte       <= 0;
+                                ctr               <= 0; 
+
                                                            
-                                   byte_0 <= ilb_byte_0; 
-                                   byte_1 <= ilb_byte_1; 
-                                   byte_2 <= ilb_byte_2; 
-                                   byte_3 <= ilb_byte_3; 
-                                   byte_4 <= ilb_byte_4; 
-                                   byte_5 <= ilb_byte_5; 
+                                   byte_0 <= 0; 
+                                   byte_1 <= 0; 
+                                   byte_2 <= 0; 
+                                   byte_3 <= 0; 
+                                   byte_4 <= 0; 
+                                   byte_5 <= 0; 
                                 
                 
-                                  if (ctr == 1) 
-                                     state <= IDLE; 
-                                  else 
-                                     ctr <= ctr + 1;   
+                               if (sop_to_ilb_rts_II) 
+                                  state <= LATCH_BYTES;
+                               else 
+                                   state <= READ_ILB_BYTES;   
                                        
                 
-                
-                               end //Latch Byte case end
+                               end //Read ILB Bytes case end
+                               
+                               
+                               
+              LATCH_BYTES:    begin
+              
+                                 sop_to_ilb_rts_I  <= 0;
+                                 sop_to_ilb_rtr_II <= 1; 
+                                 bytes_recieved    <= 1;  
+                                 output_byte       <= 0;
+                                                             
+                                 byte_0 <= ilb_byte_0; 
+                                 byte_1 <= ilb_byte_1; 
+                                 byte_2 <= ilb_byte_2; 
+                                 byte_3 <= ilb_byte_3; 
+                                 byte_4 <= ilb_byte_4; 
+                                 byte_5 <= ilb_byte_5; 
+                                  
+                    
+                                 if (ctr == 1) 
+                                    state <= IDLE;
+                                 else 
+                                    ctr <= ctr + 1;                                                          
+                    
+                                 end //Read ILB Bytes case end
+                                 
+                                
             
             endcase
        
